bd8255c39848b987efd838f79088a91c10b8e6ed
Aaron Tobey <abtobey@gmail.com>
2020-11-19 15:39:01 -0600

    added to server to try and deploy

--- server.js
+++ server.js
@@ -31,5 +31,15 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/drafts", drafts);
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
